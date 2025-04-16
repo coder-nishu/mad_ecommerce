cc8277cf18616b4f2c3c6ab762745a0a243aaebe
ahrafi16 <anjumhossainrafi16@gmail.com>
2025-04-16 16:46:44 +0600

    added all providers by ANJUM HOSSAIN

---
+++ lib/providers/login_provider.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:toastification/toastification.dart';
+
+import '../routers/app_routers.dart';
+import '../widgets/toast.dart';
+
+class LoginProvider extends ChangeNotifier {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  // SMIInput<bool>? isChecking;
+  // SMIInput<bool>? isHandsUp;
+  // SMIInput<bool>? trigSuccess;
+  // SMIInput<bool>? trigFail;
+  // SMINumber? numLook;
+  // StateMachineController? stateMachineController;
+
+  bool isPasswordVisible = false;
+  bool isLoading = false;
+
+  // void initializeRive(Artboard artBoard) {
+  //   stateMachineController = StateMachineController.fromArtboard(
+  //     artBoard,
+  //     "Login Machine",
+  //   );
+  //   if (stateMachineController == null) return;
+  //   artBoard.addController(stateMachineController!);
+  //
+  //   isChecking = stateMachineController?.findInput("isChecking");
+  //   isHandsUp = stateMachineController?.findInput("isHandsUp");
+  //   trigSuccess = stateMachineController?.findInput("trigSuccess");
+  //   trigFail = stateMachineController?.findInput("trigFail");
+  //   numLook = stateMachineController?.findSMI("numLook");
+  // }
+
+  // void isCheckField() {
+  //   isHandsUp?.change(false);
+  //   isChecking?.change(true);
+  //   numLook?.change(0);
+  // }
+
+  // void moveEyeBall(String val) {
+  //   numLook?.change(val.length.toDouble());
+  // }
+
+  // void hidePassword() {
+  //   isPasswordVisible ? isChecking?.change(true) : isHandsUp?.change(true);
+  // }
+
+  void togglePasswordVisibility() {
+    isPasswordVisible = !isPasswordVisible;
+
+    // if (isPasswordVisible) {
+    //   isChecking?.change(true);
+    //   isHandsUp?.change(false);
+    // } else {
+    //   isChecking?.change(false);
+    //   isHandsUp?.change(true);
+    // }
+
+    notifyListeners();
+  }
+
+  // Signout function
+  Future<void> logout(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      showToast(
+        context,
+        "Logout Failed",
+        e.toString(),
+        ToastificationType.error,
+      );
+    }
+  }
+
+  Future<bool> signInWithEmail(BuildContext context) async {
+    // if (!formKey.currentState!.validate()) {
+    //   trigFail?.change(true);
+    //   return false;
+    // }
+
+    try {
+      isLoading = true;
+      notifyListeners();
+
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+
+      showToast(
+        context,
+        "Login Successful",
+        'Congratulations! You have successfully logged in.',
+        ToastificationType.success,
+      );
+
+      // Then navigate to home
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        AppRoutes.home,
+            (route) => false,
+      );
+
+      emailController.clear();
+      passwordController.clear();
+      return true;
+    } on FirebaseAuthException catch (e) {
+      // trigFail?.change(true);
+
+      showToast(
+        context,
+        "Login Failed",
+        e.message ?? 'Login failed',
+        ToastificationType.error,
+      );
+
+      print("Login Error: ${e.code} - ${e.message}");
+
+      return false;
+    } finally {
+      // isChecking?.change(false);
+      // isHandsUp?.change(false);
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
---
+++ lib/providers/signup_provider.dart
@@ -0,0 +1,85 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:toastification/toastification.dart';
+
+import '../routers/app_routers.dart';
+import '../widgets/toast.dart';
+
+class SignUpProvider extends ChangeNotifier {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  bool isPasswordVisible = false;
+
+  void togglePasswordVisibility() {
+    isPasswordVisible = !isPasswordVisible;
+    notifyListeners();
+  }
+
+  Future<void> signUpWithEmail(BuildContext context) async {
+    if (!formKey.currentState!.validate()) {
+      // trigFail?.change(true);
+      return;
+    }
+
+    try {
+
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+
+      // Save additional user info to Firestore
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(credential.user!.uid)
+          .set({
+        'uid': credential.user!.uid,
+        'email': emailController.text.trim(),
+        'username': usernameController.text.trim(),
+        'address': '',
+        'profileImage': '',
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+
+      showToast(
+        context,
+        "Signup Successful",
+        'Congratulations! You have successfully signed up.',
+        ToastificationType.success,
+      );
+
+      // Then navigate to home
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        AppRoutes.home,
+            (route) => false,
+      );
+
+      emailController.clear();
+      passwordController.clear();
+      usernameController.clear();
+    } on FirebaseAuthException catch (e) {
+      print("Firebase SignUp Error: ${e.code} - ${e.message}");
+      showToast(
+        context,
+        "Signup Failed",
+        e.message ?? 'Signup failed',
+        ToastificationType.error,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
