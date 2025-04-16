dab2e1419f3b38dddd8a8af600a228b40c3e8ac2
coder-nishu <nishatkh1582@gmail.com>
2025-04-16 16:37:57 +0600

    firebase options added

---
+++ lib/app.dart
@@ -0,0 +1,42 @@
+import 'package:ecommerce/providers/bottom_nav_provider.dart';
+import 'package:ecommerce/providers/category_product_provider.dart';
+import 'package:ecommerce/views/cart_page.dart';
+import 'package:ecommerce/views/home_page.dart';
+import 'package:ecommerce/views/profile_page.dart';
+import 'package:ecommerce/views/wish_list_page.dart';
+import 'package:ecommerce/widgets/bottom_nav.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class EzzyShopSpp extends StatefulWidget {
+  const EzzyShopSpp({super.key});
+
+  static final List<Widget> pages = [
+    HomePage(),
+    WishListPage(),
+    CartPage(),
+    ProfilePage(),
+  ];
+
+  @override
+  State<EzzyShopSpp> createState() => _EzzyShopSppState();
+}
+
+class _EzzyShopSppState extends State<EzzyShopSpp> {
+
+  @override
+  Widget build(BuildContext context) {
+    final bottomNavProvider = Provider.of<BottomNavProvider>(context);
+
+    return Scaffold(
+      body: IndexedStack(
+        index: bottomNavProvider.selectedIndex,
+        children: EzzyShopSpp.pages,
+      ),
+      bottomNavigationBar: HBottomNavBar(
+        selectedIndex: bottomNavProvider.selectedIndex,
+        onItemTapped: bottomNavProvider.updateIndex,
+      ),
+    );
+  }
+}
